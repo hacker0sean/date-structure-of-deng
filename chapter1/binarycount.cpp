5fe571575e4eadadbdb5c4a36d8893721eddac71
hacker0sean <31178013+hacker0sean@users.noreply.github.com>
2017-09-14 17:03:22 +0800

    Update binarycount.cpp

--- chapter1/binarycount.cpp
+++ chapter1/binarycount.cpp
@@ -1,13 +1,4 @@
-#include<iostream>
-using namespace std;
-int countOnes(unsigned int n);
-int main()
-{
-    cout<<countOnes(441);
-    return 0;
-}
-
-int countOnes(unsigned int n)
+int countOnes(unsigned int n)//O£¨logn£©
 {
     int ones=0;
     while(n>0)
@@ -16,4 +7,26 @@ int countOnes(unsigned int n)
         n>>=1;
     }
     return ones;
+}
+
+int countOnes(unsigned int n)//O£¨countones£¨n£©£©
+{
+    int ones=0;
+    while(0<n)
+        ones++;
+    n&=n-1;
+    return ones;
+}
+
+#define POW(c)(1<<(c))
+#define MASK(c)  (((unsigned long)-1)/(POW(POW(c))+1)
+#define ROUND(n,c) (((n) & MASK(c))+((n)>>POW(c) & MASK(c)))
+int countOnes(unsigned int n)//O(log2W)
+{
+    n=ROUND(n,0);
+    n=ROUND(n,2);
+    n=ROUND(n,3);
+    n=ROUND(n,4);
+    n=ROUND(n,5);
+    return n;
 }
